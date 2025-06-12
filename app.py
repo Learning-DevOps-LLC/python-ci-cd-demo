d99613fd7915242773f1ad3cfcb57726b2719b49
ASHISHs21 <ksinghashish357@gmail.com>
2025-06-12 11:13:59 +0000

    updated appa nd test

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD Pipeline!"
+    return "Hello from CI/CD Pipeline!<br>Ashish Singh"
+
+@app.route('/health')
+def health():
+    return {"status": "ok", "message": "Service is healthy"}, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
