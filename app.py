3481f166668f4e0d7a6c240a32f884fedda1461b
ASHISHs21 <ksinghashish357@gmail.com>
2025-06-12 10:19:50 +0000

    Add CI test and Docker push pipeline

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD Pipeline!"
+    return '''Hello from CI/CD Pipeline!
+     Ashish Singh'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
