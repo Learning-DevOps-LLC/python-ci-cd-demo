966d1c488cfce4a05beae348b242fc84f285544c
ASHISHs21 <ksinghashish357@gmail.com>
2025-06-12 11:19:01 +0000

    updated appa nd test

--- test_app.py
+++ test_app.py
@@ -1,6 +1,17 @@
+import pytest
 from app import app
 
-def test_home():
-    client = app.test_client()
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+def test_home(client):
     response = client.get('/')
-    assert response.data == b"Hello from CI/CD Pipeline!"
+    assert response.status_code == 200
+    assert b"Hello from CI/CD Pipeline!<br>Ashish Singh" in response.data
+
+def test_health(client):
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.json == {"status": "ok", "message": "Service is healthy"}
