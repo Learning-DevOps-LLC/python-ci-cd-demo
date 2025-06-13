9f7f0008fe1a28aa495f8d898fd40470338e0e06
ASHISHs21 <ksinghashish357@gmail.com>
2025-06-13 04:51:36 +0000

    Update in code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    server_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template("index.html", server_time=server_time)
 
 @app.route('/health')
 def health():
